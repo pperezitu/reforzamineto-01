94bc36345b0fd90c258a42c4ac450b4e3c141f84
pedro <pedrodanielpereziturriaga@MacBook-Air-de-Pedro.local>
2022-04-27 09:53:40 -0400

    primer commit

---
+++ src/typescript/TiposBasicos.tsx
@@ -0,0 +1,10 @@
+import { Fragment } from "react"
+
+export const TiposBasicos = () => {
+    console.log('estamos desarrollando con React');
+    return (
+        <Fragment>
+            <h3>Tipos basicos</h3>
+        </Fragment>
+    )
+}
